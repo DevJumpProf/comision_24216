d3e09bf6e66090102a62c059c0138e887d0c1858
Gabriel Muñoz <gabriel.munoz@arbusta.net>
2024-05-02 17:34:40 -0300

    clasejs

---
+++ CLASES/Clase14/clase1.js
@@ -0,0 +1,62 @@
+/* let nombre; */   //UNDEFINED
+//scope
+var apellido = "Benitez"
+const dato = "Constante"
+/* nombre = "Juan" */
+
+/* dato = "cambio" */
+
+// console.log (nombre) comentario
+/* nombre = "Juan"
+console.log(nombre) */
+
+/* let mensaje = "escribo el documento"
+document.write (mensaje) */  /* escribo el documento */
+
+/* tipos de datos */
+
+let cadena = "esto es una cadena de texto"  /* = STRING */
+let numero = 3      /*  NUMBER */
+/* let num2 = "3" */
+let booleano = true /* false *//*  BOOLEAN */
+
+// interacciones
+
+/* alert ("Soy un alerta") Mensaje emergente */
+
+/* let nombre = prompt ("Decime tu nombre")  */// pide un dato y lo guarda como string
+/* let num1 = parseInt(prompt ("Decime un numero"))
+let num2= parseInt( prompt ("Decime otro numero")) */
+
+/* let num1 = Number(prompt ("Decime un numero"))
+let num2= Number( prompt ("Decime otro numero")) */
+
+/* alert (nombre + " la suma de tus numeros es : " + (num1+num2)) */ // concatenacion
+/* 
+console.log("10"*"10");
+ */
+
+let equipo = "Argentina"
+
+console.log(equipo);
+console.log(2+2);
+console.log("Hola Mundo");
+
+console.log(typeof equipo);
+console.log(typeof numero);
+
+console.log(Number.isInteger(numero));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
